7245d6902eb53db8bbf372224f2525ba55b79522
Joshua Küpper <scrayos@justchunks.net>
2014-02-05 23:19:40 +0100

    Update PlayerInServer.java

--- ProxyTablistVariables-PlayerInServer/src/main/java/PlayerInServer.java
+++ ProxyTablistVariables-PlayerInServer/src/main/java/PlayerInServer.java
@@ -58,15 +58,11 @@ public class PlayerInServer implements Variable {
             name.append(ProxyTablist.getInstance().getConfig().getString("variable.player.prefix." + p.getName(), ""));
         }
 
-        String last = null;
-
         for (String c : new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "l", "m", "n", "o", "k", "r"}) {
-            last = (p.hasPermission("proxy.tablist." + c)) ? c : last;
-        }
-
-        if(last != null) {
-            name.append("§");
-            name.append(last);
+            if (p.hasPermission("proxy.tablist." + c)) {
+                name.append("§");
+                name.append(c);
+            }
         }
 
         name.append(p.getName());
